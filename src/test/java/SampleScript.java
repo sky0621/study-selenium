02122d7a0f8547ea6a5ea6884174c60b3e6476d2
S.S <ss@ss.com>
2016-06-12 03:07:31 +0900

    chap3 list3.3

---
+++ src/test/java/SampleScript.java
@@ -0,0 +1,19 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Created by SS on 2016/06/12.
+ */
+public class SampleScript {
+
+    public static void main(String... args) {
+
+        WebDriver driver = new FirefoxDriver();
+        driver.get("http://example.selenium.jp/reserveApp");
+        driver.findElement(By.id("guestname")).sendKeys("サンプルユーザ");
+        driver.findElement(By.id("goto_next")).click();
+        driver.quit();
+    }
+
+}
